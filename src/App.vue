d66be6a53bc51a7a40dc6fd5320814959f4c1352
zofia-ela <jiangdongfeng01@gmail.com>
2021-05-16 10:01:48 +0800

    提交信息

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <div id="app">
+    <div>{{result}}</div>
    <h2>Hello</h2>
    <h3>fuze</h3>
-    <router-view/>
+    <!-- <router-view/> -->
   </div>
 </template>
 
+<script>
+import axios from 'axios'
+export default {
+  name:'App',
+  data(){
+    return{
+      result:'hello'
+    }
+  },
+  created(){
+    axios({
+      url:'http://123.207.32.32:8000/home/multidata'
+    }).then(res=>{
+      // console.log(res),
+      this.result=res
+    })
+  }
+    
+  
+}
+</script>
 <style>
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,50 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  // router,
   store,
+  axios,
   render: h => h(App)
 }).$mount('#app')
+
+
+// axios({
+//   url:'http://123.207.32.32:8000/home/multidata'
+// }).then(res=>{
+//   console.log(res)
+// })
+// axios.defaults.baseURL='http://123.207.32.32:8000'
+// axios.defaults.timeout=5000
+// axios.all([axios({
+//   url:'/home/multidata'
+// }),axios({
+//   url:'/home/multidata'
+// })]).then(result=>{
+//   console.log(result)
+// })
+
+// const instance=axios.create({
+//   baseURL:'http://123.207.32.32:8000'
+// })
+
+// instance({
+//   url:'/home/multidata'
+// }).then(res=>{
+//   console.log(res)
+// })
+
+import {request} from './network/request'
+
+request({
+  url:'/home/multidata'
+}).then(res=>{
+  console.log(res)
+}).catch(err=>{
+  console.log(err)
+})
